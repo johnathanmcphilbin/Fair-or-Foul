a79b322de6044a76dbb0192a26fcbd5704d9841b
johnathanmcphilbin <johnathanmcphilbin2@gmail.com>
2025-08-13 17:09:26 +0100

    call types

---
+++ src/fairorfoul/config.py
@@ -0,0 +1,28 @@
+CALL_TYPES = {
+    "basketball": [
+        "foul",
+        "travel",
+        "double_dribble",
+        "charge",
+        "block",
+        "shooting_foul",
+        "technical",
+        "out_of_bounds",
+    ],
+    "soccer": [
+        "foul",
+        "yellow_card",
+        "red_card",
+        "offside",
+        "penalty",
+        "handball",
+        "out_of_play",
+    ],
+    "martial_arts": [
+        "warning",
+        "point_deduction",
+        "disqualification",
+        "stalling",
+        "illegal_move",
+    ],
+}
